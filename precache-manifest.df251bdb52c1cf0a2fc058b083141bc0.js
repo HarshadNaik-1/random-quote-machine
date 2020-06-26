841e7279699a36d6fb6282a071af64f8853176f3
Harshad Naik <naikharshad01@gmail.com>
2020-06-26 16:57:30 +0530

    Updates

---
+++ precache-manifest.df251bdb52c1cf0a2fc058b083141bc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d307f634db477b42951b8f6fa7f1c5b9",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "e7f07f22a0d5ea690a17",
+    "url": "/random-quote-machine/static/css/main.e8727ae2.chunk.css"
+  },
+  {
+    "revision": "bb14446d83a8958410fb",
+    "url": "/random-quote-machine/static/js/2.fcd187fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.fcd187fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7f07f22a0d5ea690a17",
+    "url": "/random-quote-machine/static/js/main.2e6520e4.chunk.js"
+  },
+  {
+    "revision": "ee65d0d01fb70dba3e33",
+    "url": "/random-quote-machine/static/js/runtime-main.c85ea385.js"
+  }
+]);
