3ade789fa71e32b096f85993f2c5b17da03f19bd
Harshad Naik <naikharshad01@gmail.com>
2020-10-09 21:16:20 +0530

    window reload removed added component reload on button refresh

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,28 +3,41 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faQuoteLeft } from "@fortawesome/free-solid-svg-icons";
 import "./Quote.css";
 
-const refreshPage = () => {
-  setTimeout(() => {
-    window.location.reload(false);
-  }, 1000);
-};
+// const refreshPage = () => {
+//   setTimeout(() => {
+//     window.location.reload(false);
+//   }, 0);
+// };
+
+let randomNum = Math.floor(Math.random() * 1500 + 1);
+let randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+
 const url = "https://type.fit/api/quotes";
-const randomNum = Math.floor(Math.random() * 1500 + 1);
-const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
 
 const Quote = () => {
   const [quote, setQuote] = useState(null);
   const [author, setAuthor] = useState(null);
+  const [randomNumber, setRandomNumber] = useState(randomNum);
+  const [randomBodyColor, setRandomBodyColor] = useState(randomColor);
+
+  const refreshPage = () => {
+    let randomN = Math.floor(Math.random() * 1500 + 1);
+    let randomC = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    setRandomNumber((randomNumber) => randomN);
+    setRandomBodyColor((randomBodyColor) => randomC);
+  };
+
   useEffect(() => {
     const effect = async () => {
       const res = await fetch(url);
       const data = await res.json();
-      setQuote(data[randomNum].text);
-      setAuthor(data[randomNum].author);
-      document.body.style.backgroundColor = randomColor;
+      setQuote(data[randomNumber].text);
+      setAuthor(data[randomNumber].author);
+      document.body.style.backgroundColor = randomBodyColor;
     };
     effect();
-  });
+  }, [randomNumber, randomBodyColor]);
+
   if (!quote || !author)
     return (
       <h1
@@ -35,20 +48,38 @@ const Quote = () => {
         }}
       >
         <FontAwesomeIcon icon={faQuoteLeft} />
+        <div className='btnbox'>
+          <button
+            className='btn'
+            style={{
+              transition: "ease-in",
+              backgroundColor: "rgb(163, 165, 250)",
+            }}
+            onClick={refreshPage}
+          >
+            Reload{" "}
+          </button>
+        </div>
       </h1>
     );
   return (
     <div>
-      <h1 style={{ color: randomColor, transition: "ease-in" }}>{quote}</h1>
+      <h1 style={{ color: randomBodyColor, transition: "ease-in" }}>
+        <FontAwesomeIcon icon={faQuoteLeft} /> {quote}
+      </h1>
       <p
-        style={{ color: randomColor, textAlign: "right", paddingLeft: "20px" }}
+        style={{
+          color: randomBodyColor,
+          textAlign: "right",
+          paddingLeft: "20px",
+        }}
       >
         ~ {author}
       </p>
-      <div className="btnbox">
+      <div className='btnbox'>
         <button
-          className="btn"
-          style={{ backgroundColor: randomColor }}
+          className='btn'
+          style={{ backgroundColor: randomBodyColor }}
           onClick={refreshPage}
         >
           New Quote
